810e23a15d4edb23133bb7c0b82caf45b25d50eb
Aaron Rumery <bloodhawk@users.noreply.github.com>
2014-10-30 15:11:24 -0600

    fix swal

--- public/app/controllers/ProfileCtrl.js
+++ public/app/controllers/ProfileCtrl.js
@@ -20,8 +20,13 @@ app.controller('ProfileCtrl', function($scope, userSvc, userData, ModalService){
              	return;
              } else {
              	game.avail = times;
-             	userSvc.postGame(game).then(function(){
-             		console.log('hello');
+             	userSvc.postGame(game).then(function(updated){
+             		console.log(typeof updated);
+             		if(updated === 'false'){
+             			swal("Oops...", "You've already added this game before", "error");
+             		} else {
+             			swal("Woot!", "Your game has been added!", "success")
+             		}
              	});
              }
           });
