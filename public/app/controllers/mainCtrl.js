dbfced481a592dfb1159535d57eb2849cc5b25df
Aaron Rumery <bloodhawk@users.noreply.github.com>
2014-11-08 15:04:41 -0700

    css changes to findgamer page

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,9 +1,12 @@
 app.controller('mainctrl', function($scope, $location){
-	var cssClass = 'steam-back';
 	$scope.isActive = function() {
 		return $location.path() === '/login';
 	};
 	$scope.bgimage = function(){
-		if($location.path() === '/login') return cssClass;
-	}
+		return $location.path() === '/login';
+	};
+
+	$scope.bg = function(){
+		return $location.path() === '/findgamer';
+	};
 });
